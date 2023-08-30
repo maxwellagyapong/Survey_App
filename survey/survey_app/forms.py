f328de3c1c01dbc56d4f0c7fa9b9d2bf9137d11d
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-08-30 17:51:53 +0100

    Created NumberQuestionForm

--- survey/survey_app/forms.py
+++ survey/survey_app/forms.py
@@ -15,3 +15,10 @@ class TextQuestionForm(forms.ModelForm):
     class Meta:
         model = TextQuestion
         fields = ("label", "is_required", "input_length",)
+
+
+class NumberQuestionForm(forms.ModelForm):
+
+    class Meta:
+        model = NumberQuestion
+        fields = ("label", "is_required", "min_value", "max_value")
