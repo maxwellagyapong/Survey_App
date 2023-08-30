fca44daeaed4d3bd1cfebf912493aa93d53d5b4c
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-08-30 15:16:31 +0100

    Created Survey model

--- survey/survey_app/models.py
+++ survey/survey_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Survey(models.Model):
+    name = models.CharField(_("Survey Name"), max_length=255, unique=True)
+    dt_created = models.DateTimeField(_("Date/Time Created"), auto_now_add=True,)
+    dt_update = models.DateTimeField(_("Date/Time Updated"), auto_now=True)
+    slug = models.SlugField(_("Slug"), blank=True, default="")
+    
+    def __str__(self) -> str:
+        return self.name
