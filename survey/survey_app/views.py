a16eb35489399903217fab7269730af7c34b4a5f
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-08-30 17:35:42 +0100

    Registered models to admin site

--- survey/survey_app/views.py
+++ survey/survey_app/views.py
@@ -1,6 +1,3 @@
-from django.contrib import admin
-from .models import Survey, SurveySubmission
-
-
-admin.site.register(Survey)
-admin.site.register(SurveySubmission)
+from django import forms
+from .models import (Survey, TextQuestion, NumberQuestion, 
+                     SingleSelectQuestion, SingleSelectOptions,)
