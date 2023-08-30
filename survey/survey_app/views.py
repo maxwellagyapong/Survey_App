624f895f49a78f08dfeaf1c289435eec751fde5a
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-08-30 20:20:52 +0100

    Created SurveyDetailView

--- survey/survey_app/views.py
+++ survey/survey_app/views.py
@@ -11,3 +11,18 @@ from django.views import generic
 class SurveyListView(generic.ListView):
     queryset = Survey.objects.all()
     template_name = ""
+    
+    
+class SurveyDetailView(generic.DeleteView):
+    template_name = ""
+    
+    def get_queryset(self):
+        slug = self.kwargs.get('slug', None)
+        
+        if slug == None:
+            messages.error(self.request, _(
+            "The survey you trying to access does not exist."))
+            return redirect("survey_list")
+            
+        return Survey.objects.get(slug=slug)
+    
