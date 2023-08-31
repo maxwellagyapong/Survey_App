ede188aefbf377e0cad37a2707e6a72c56ac172f
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-08-31 07:29:33 +0100

    Configured endpoint for SurveyCreateView

--- survey/survey_app/urls.py
+++ survey/survey_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SurveyListView
+from .views import *
 
 urlpatterns = [
-    path('welcome/', SurveyListView.as_view(), name="survey_list")
+    path('welcome/', SurveyListView.as_view(), name="survey_list"),
+    path("create/", SurveyCreateView.as_view(), name="survey_create"),
 ]
