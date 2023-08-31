0eac1118318db97ae21e26a6fb7ce8b42c0552a2
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-08-31 08:29:28 +0100

    Created enpoint for Survey Deteail

--- survey/survey_app/urls.py
+++ survey/survey_app/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
 
 urlpatterns = [
     path('welcome/', SurveyListView.as_view(), name="survey_list"),
     path("create/", SurveyCreateView.as_view(), name="survey_create"),
+    path("<slug:slug>/", include([
+        path("", SurveyDetail.as_view(), name="survey_detail"),]))
 ]
