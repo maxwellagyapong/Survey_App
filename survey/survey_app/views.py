2f32117e6230811575e8719f77607a8f6ad47ff0
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-08-31 20:25:31 +0100

    Created view for updating image question

--- survey/survey_app/views.py
+++ survey/survey_app/views.py
@@ -478,4 +478,48 @@ def image_question_create(request, slug):
     }
 
     return render(request, "survey/ImageQuestionCreate.html", context)
+
+
+def image_question_update(request, slug, id):
+    try:
+        survey: Survey | None = Survey.objects.filter(slug=slug).first()
+    except Survey.DoesNotExist:
+        survey = None
+
+    if survey is None:
+        messages.error(request, _(
+            "The survey you are trying to update does not exist."))
+        return redirect("survey_list")
+
+    try:
+        image_question: ImageQuestion | None = ImageQuestion.objects.filter(
+            id=id).first()
+    except ImageQuestion.DoesNotExist:
+        image_question = None
+
+    if image_question is None:
+        messages.error(request, _(
+            "The question you are trying to update does not exist."))
+        return redirect("survey_detail", slug)
+
+    image_question_form = TextQuestionForm(
+        request.POST or None,
+        instance=image_question
+    )
+
+    if image_question_form.is_valid():
+        image_question_form.save()
+        messages.success(request, _(
+            f"Question has successfully updated to survey {survey.name}."))
+        return redirect("survey_detail", slug)
+
+    context = {
+        "survey": survey,
+        "image_question": image_question,
+        "image_question_form": image_question_form
+    }
+
+    return render(request, "survey/ImageQuestionUpdate.html", context)
+    
+    
     
