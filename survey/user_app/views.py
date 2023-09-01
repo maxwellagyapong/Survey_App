dc82679928abf185fa350dca07995b337f65e8d1
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-09-01 11:44:28 +0100

    Implemented basic login for root user

--- survey/user_app/views.py
+++ survey/user_app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import CustomUser
+from .forms import LoginForm
+from django.contrib import messages
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST, None)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            
+            if username == 'master' and password == 'keypass':
+                request.session['is_authenticated'] = True
+                return redirect('survey_list')  
+            else:
+                messages.error(request, 'Invalid credentials. Please try again.')
+        
+    context = {
+        "form": form
+    }
+    
+    return render(request, 'user/login.html', context)
