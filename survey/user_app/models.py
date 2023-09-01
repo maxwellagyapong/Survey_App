c3042a14375725f3cdb2a078896c6c2dc482efb9
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-09-01 11:29:12 +0100

    Created Custom user model

--- survey/user_app/models.py
+++ survey/user_app/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser):
-    pass
+class CustomUser(models.Model):
+    username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.username
