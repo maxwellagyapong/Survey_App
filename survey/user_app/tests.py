83f8055ef943aa65caab37a6e8f88e0f126e5894
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-09-03 18:21:14 +0100

    Created user testcase

--- survey/user_app/tests.py
+++ survey/user_app/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import User
+from django.urls import reverse
 
-# Create your tests here.
+class UserTestCase(TestCase):
+    
+    def setUp(self) -> None:
+        User.objects.create_user(username='test', password='password')
+        
+    def test_user_login(self):
+        
+        data = {
+            'username':'test',
+            'password':'password'
+        }
+        
+        response = self.client.post(reverse('login'), data)
+        self.assertTrue(response.status_code, 200)
